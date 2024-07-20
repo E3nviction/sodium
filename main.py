bf09136e620e71ae9d7330dc02f49f09f6e46b9c
Enviction <schmidtjonas662@gmail.com>
2024-07-20 18:41:08 +0200

    tested some functions and the examples in README

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ sodium.init()
 window = Window(640, 480)
 win = window.get_screen()
 
-# this is the function that will be called when the button is pressed
-def on_button_press():
-    button.set_text("Goodbye, World!") # change the button's text to "Goodbye, World!"
+def toggle_button_2():
+    if button_2.is_disabled(): # if the second button is disabled
+        button_2.enable() # enable the second button
+    else: # if the second button is not disabled
+        button_2.disable() # disable the second button
+# or...
+def toggle_button_2_v2():
+    button_2.set_disabled(not button_2.is_disabled())
+# or...
+def toggle_button_2_v3():
+    button_2.toggle_disabled()
 
-# create the button and set the on_click_func to the on_button_press function
-button = Button(win, "Hello, World!", (50, 50, 150, 50), on_click_func=on_button_press) 
+# create the button with the on_click_func to toggle the second button
+button = Button(win, "Toggle, it!", (50, 50, 150, 50), on_click_func=toggle_button_2)
+
+button_2 = Button(win, "Toggle, me!", (50, 150, 150, 50), disabled=True) # create the second button
+button_2.set_on_click_func(lambda: print("I'm Alive!!!")) # set the on_click_func to print "I'm Alive!!!"
 
 def main():
     button.update() # update the button
+    button_2.update() # update the second button
 sodium.start(win, main)
