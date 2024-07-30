e36bf84be4847122455aacc1d70f267241541433
Enviction <schmidtjonas662@gmail.com>
2024-07-30 13:56:59 +0200

    added error checking messages

--- install.sh
+++ install.sh
@@ -5,10 +5,33 @@ if [ "$EUID" -ne 0 ]
     exit
 fi
 
-rm README.md
-rm LICENSE
-rm changelog.md
-rm TODO.md
-mv sodium/* .
-rm sodium/ -d
-rm install.sh
+echo "installing sodium..."
+rm README.md > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing README.md"
+fi
+rm LICENSE > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing LICENSE"
+fi
+rm changelog.md > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing changelog.md"
+fi
+rm TODO.md > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing TODO.md"
+fi
+mv sodium/* . > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while moving files"
+fi
+rm -r sodium > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing sodium folder"
+fi
+echo "successfully installed sodium"
+rm install.sh > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error occurred while removing install.sh"
+fi
