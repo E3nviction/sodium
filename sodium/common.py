25066ddd25d1e080f01e49aeffbdd0e28513d0f6
Enviction <schmidtjonas662@gmail.com>
2024-08-06 15:42:59 +0200

    added function to common module for creating an id

--- sodium/common.py
+++ sodium/common.py
@@ -4,28 +4,18 @@ common module
 import pygame
 from .constants import Color
 from . import theme
+from . import cache
 
 def set_color(color, default):
-    """
-    Sets the color
-
-    :param color: Color
-    :param default: str
-    :return: Color
-    """
     theme_colors = theme.get_theme()
     color = Color(theme_colors[default]) if color == "theme" else Color(color)
     return color
 
 def set_font(font, font_size):
-    """
-    Sets the font
-
-    :param font: str
-    :param font_size: int
-    :return: str
-    """
     theme_colors = theme.get_theme()
     default_font = pygame.font.Font(theme_colors["FONT"], theme_colors["FONT_SIZE"])
     font = default_font if font is None or font == "theme" else pygame.font.Font(font, font_size)
     return font
+
+def widget_get_id():
+    return len(cache.widgets) + 1
