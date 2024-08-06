6650d02c84ebcc5ca193b3e4e05ac2c1cb8135c8
Enviction <schmidtjonas662@gmail.com>
2024-08-06 14:05:25 +0200

    rewrote button's methods as well as sorted and polished them

--- sodium/widgets/button.py
+++ sodium/widgets/button.py
@@ -5,10 +5,8 @@ import pygame
 from .. import constants, common
 from .label import Label
 
+  # pylint: disable=missing-docstring
 class Button:
-    """
-    A button widget that displays text and is clickable with a mouse button, aswell as being able to be disabled and enabled.
-    """
     def __init__(
         self,
         surface,
@@ -32,30 +30,6 @@ class Button:
         on_click_args=None,
         mouse_button=0
         ):
-        """
-        Initializes the button
-
-        :param surface: Surface
-        :param text: str
-        :param rect: tuple
-        :param color: Color
-        :param background: Color
-        :param hover_color: Color
-        :param hover_background: Color
-        :param active_color: Color
-        :param active_background: Color
-        :param disabled_color: Color
-        :param disabled_background: Color
-        :param font: Font
-        :param font_family: str
-        :param font_size: int
-        :param label_horizontal: str [left, center, right]
-        :param label_vertical: str [top, center, bottom]
-        :param disabled: bool
-        :param on_click_func: function
-        :param on_click_args: list
-        :param mouse_button: int
-        """
         super().__init__()
         self.surface = surface
         self.text = text
@@ -76,273 +50,165 @@ class Button:
         self.disabled = disabled
         self.on_click_func = on_click_func
         self.on_click_args = on_click_args
-        self.locked = False
         self.mouse_button = mouse_button
+
+        self.locked = False
+        self.button_state = None
 
         if font is None:
             self.font = common.set_font(font_family, font_size)
 
-    def is_touching_mouse(self):
-        """
-        Checks if the mouse is touching the button
-
-        :return: bool
-        """
+    def is_touching_mouse(self) -> bool:
         return self.rect.collidepoint(pygame.mouse.get_pos()) and not self.disabled
-
-    def is_active(self):
-        """
-        Checks if the button is active
-
-        :return: bool
-        """
+    def is_active(self) -> bool:
         return self.is_touching_mouse() and pygame.mouse.get_pressed()[self.mouse_button] and not self.disabled
-
-    def is_locked(self):
-        """
-        Checks if the button is locked
-
-        :return: bool
-        """
+    def is_locked(self) -> bool:
         return self.locked
-
-    def is_disabled(self):
-        """
-        Checks if the button is disabled
-
-        :return: bool
-        """
+    def is_disabled(self) -> bool:
         return self.disabled
 
-    def set_on_click_func(self, func):
-        """
-        Sets the button's on click function
-
-        :param func: function
-        """
+    def get_on_click_func(self) -> tuple:
+        return self.on_click_func
+    def set_on_click_func(self, func) -> None:
         self.on_click_func = func
 
-    def set_text(self, text):
-        """
-        Sets the button's text
-
-        :param text: str
-        """
+    def get_text(self) -> str:
+        return self.text
+    def set_text(self, text) -> None:
         self.text = text
 
-    def get_text(self):
-        """
-        Gets the button's text
-
-        :return: str
-        """
-        return self.text
-    
-    def set_on_click_args(self, args):
-        """
-        Sets the button's on click arguments
-
-        :param args: list
-        """
+    def get_on_click_args(self) -> tuple:
+        return self.on_click_args
+    def set_on_click_args(self, args) -> None:
         self.on_click_args = args
 
-    def get_on_click_args(self):
-        """
-        Gets the button's on click arguments
-
-        :return: list
-        """
-        return self.on_click_args
-
-    def get_rect(self):
-        """
-        Gets the button's rect
-
-        :return: Rect
-        """
-        return self.rect
-
-    def get_width(self):
-        """
-        Gets the button's width
-
-        :return: int
-        """
+    def get_width(self) -> int:
         return self.rect.width
+    def set_width(self, width) -> None:
+        self.rect.width = width
 
-    def get_height(self):
-        """
-        Gets the button's height
-
-        :return: int
-        """
+    def get_height(self) -> int:
         return self.rect.height
+    def set_height(self, height) -> None:
+        self.rect.height = height
 
-    def set_rect(self, rect):
-        """
-        Sets the button's rect
-
-        :param rect: Rect
-        """
+    def get_rect(self) -> tuple:
+        return self.rect
+    def set_rect(self, rect) -> None:
         self.rect = constants.Rect(rect)
 
-    def get_size(self):
-        """
-        Gets the Button's size
-
-        :return: tuple
-        """
+    def get_size(self) -> tuple:
         return self.rect.size
-    
-    def set_size(self, size):
-        """
-        Sets the Button's size
-
-        :param size: tuple
-        """
+    def set_size(self, size) -> None:
         self.rect.size = size
-    
-    def get_x(self):
-        """
-        Gets the Button's x
 
-        :return: int
-        """
+    def get_x(self) -> int:
         return self.rect.x
-    
-    def get_y(self):
-        """
-        Gets the Button's y
-
-        :return: int
-        """
-        return self.rect.y
-
-    def set_x(self, x):
-        """
-        Sets the Button's x
-
-        :param x: int
-        """
+    def set_x(self, x) -> None:
         self.rect.x = x
 
-    def set_y(self, y):
-        """
-        Sets the Button's y
-
-        :param y: int
-        """
+    def get_y(self) -> int:
+        return self.rect.y
+    def set_y(self, y) -> None:
         self.rect.y = y
-    
-    def get_position(self):
-        """
-        Gets the Button's position
 
-        :return: tuple
-        """
+    def get_position(self) -> tuple:
         return self.rect.topleft
-
-    def set_position(self, position):
-        """
-        Sets the Button's position
-
-        :param position: tuple
-        """
+    def set_position(self, position) -> None:
         self.rect.topleft = position
 
-    def set_color(self, color):
-        """
-        Sets the button's color
-
-        :param color: Color
-        """
+    def get_color(self) -> str:
+        return self.color
+    def set_color(self, color) -> None:
         self.color = common.set_color(color, "FOREGROUND")
 
-    def set_background(self, color):
-        """
-        Sets the button's background
-
-        :param color: Color
-        """
+    def get_background(self) -> str:
+        return self.background
+    def set_background(self, color) -> None:
         self.background = common.set_color(color, "ACCENT")
 
-    def set_disabled(self, disabled):
-        """
-        Sets the button's disabled
-
-        :param disabled: bool
-        """
-        self.disabled = disabled
-    
-    def get_disabled(self):
-        """
-        Gets the Button's disabled status
-
-        :return: bool
-        """
+    def get_disabled(self) -> bool:
         return self.disabled
+    def set_disabled(self, disabled) -> None:
+        self.disabled = disabled
 
-    def set_disabled_color(self, color):
-        """
-        Sets the button's disabled color
-
-        :param color: Color
-        """
+    def get_disabled_color(self) -> str:
+        return self.disabled_color
+    def set_disabled_color(self, color) -> None:
         self.disabled_color = common.set_color(color, "FOREGROUND_DISABLED")
 
-    def set_disabled_background(self, color):
-        """
-        Sets the button's disabled background
-
-        :param color: Color
-        """
+    def get_disabled_background(self) -> str:
+        return self.disabled_background
+    def set_disabled_background(self, color) -> None:
         self.disabled_background = common.set_color(color, "ACCENT_DISABLED")
 
-    def set_hover_color(self, color):
-        """
-        Sets the button's hover color
-
-        :param color: Color
-        """
+    def get_hover_color(self) -> str:
+        return self.hover_color
+    def set_hover_color(self, color) -> None:
         self.hover_color = common.set_color(color, "FOREGROUND")
 
-    def set_hover_background(self, color):
-        """
-        Sets the button's hover background
-
-        :param color: Color
-        """
+    def get_hover_background(self) -> str:
+        return self.hover_background
+    def set_hover_background(self, color) -> None:
         self.hover_background = common.set_color(color, "ACCENT_HOVER")
 
-    def set_active_color(self, color):
-        """
-        Sets the button's active color
-
-        :param color: Color
-        """
+    def get_active_color(self) -> str:
+        return self.active_color
+    def set_active_color(self, color) -> None:
         self.active_color = common.set_color(color, "FOREGROUND")
 
-    def set_active_background(self, color):
-        """
-        Sets the button's active background
-
-        :param color: Color
-        """
+    def get_active_background(self) -> str:
+        return self.active_background
+    def set_active_background(self, color) -> None:
         self.active_background = common.set_color(color, "ACCENT_ACTIVE")
 
-    def set_font(self, font_family, font_size):
-        """
-        Sets the button's font
+    def get_locked(self) -> bool:
+        return self.locked
+    def set_locked(self, locked) -> None:
+        self.locked = locked
 
-        :param font: str
-        :param font_size: int
-        """
+    def get_label_horizontal(self) -> str:
+        return self.label_horizontal
+    def set_label_horizontal(self, horizontal) -> None:
+        self.label_horizontal = horizontal
+    def get_label_vertical(self) -> str:
+        return self.label_vertical
+    def set_label_vertical(self, vertical) -> None:
+        self.label_vertical = vertical
+
+    def get_font(self) -> str:
+        return self.font
+    def set_font(self, font_family, font_size) -> None:
         self.font = common.set_font(font_family, font_size)
 
-    def on_click(self):
-        """
-        Calls the button's on click function
-        """
+    def enable(self) -> None:
+        self.disabled = False
+    def disable(self) -> None:
+        self.disabled = True
+
+    def toggle_disabled(self) -> None:
+        self.disabled = not self.disabled
+
+    def get_mouse_button(self) -> int:
+        return self.mouse_button
+    def set_mouse_button(self, mouse_button) -> None:
+        self.mouse_button = mouse_button
+
+    def get_state(self) -> str:
+        if self.button_state is None:
+            return "normal"
+        if self.button_state:
+            return "pressed"
+        else:
+            return "released"
+    def set_state(self, state) -> None:
+        if state == "normal":
+            self.button_state = None
+        elif state == "pressed":
+            self.button_state = True
+        elif state == "released":
+            self.button_state = False
+    def check_click(self) -> bool:
         if self.is_active() and not self.is_locked():
             self.locked = True
             if self.on_click_func:
@@ -350,122 +216,33 @@ class Button:
                     self.on_click_func(*self.on_click_args)
                 else:
                     self.on_click_func()
+            self.button_state = True
+            return True
+        if not self.is_active() and self.is_locked():
+            self.locked = False
+            self.button_state = False
+            return False
         if not self.is_active():
             self.locked = False
-
-    def set_label_horizontal(self, horizontal):
-        """
-        Sets the button's label horizontal
-
-        :param horizontal: str
-        """
-        self.label_horizontal = horizontal
-
-    def set_label_vertical(self, vertical):
-        """
-        Sets the button's label vertical
-
-        :param vertical: str
-        """
-        self.label_vertical = vertical
-    
-    def set_locked(self, locked):
-        """
-        Sets the button's locked
-
-        :param locked: bool
-        """
-        self.locked = locked
-
-    def get_label_horizontal(self):
-        """
-        Gets the button's label horizontal
-
-        :return: str
-        """
-        return self.label_horizontal
-
-    def get_label_vertical(self):
-        """
-        Gets the button's label vertical
-
-        :return: str
-        """
-        return self.label_vertical
-
-    def get_font(self):
-        """
-        Gets the button's font
-
-        :return: str
-        """
-        return self.font
-    
-    def enable(self):
-        """
-        Enables the button
-        """
-        self.disabled = False
-
-    def disable(self):
-        """
-        Disables the button
-        """
-        self.disabled = True
-    
-    def toggle_disabled(self):
-        """
-        Toggles the button's disabled
-        """
-        self.disabled = not self.disabled
-    
-    def get_mouse_button(self):
-        """
-        Gets the button's mouse button
-
-        :return: int
-        """
-        return self.mouse_button
-
-    def set_mouse_button(self, mouse_button):
-        """
-        Sets the button's mouse button
-
-        :param mouse_button: int
-        """
-        self.mouse_button = mouse_button
-
-    def draw(self):
-        """
-        Draws the button
-        """
-        disabled_color = self.disabled_color
-        surface = self.surface
-        label_horizontal = self.label_horizontal
-        label_vertical = self.label_vertical
-        text = self.text
-        rect = self.rect
-        font = self.font
+            self.button_state = None
+            return None
+    def draw(self) -> None:
         if self.is_active() and not self.is_disabled():
-            pygame.draw.rect(surface, self.active_background, self.rect)
-            Label(surface, text, rect, self.active_color, font, align_horizontal=label_horizontal, align_vertical=label_vertical).draw()
+            pygame.draw.rect(self.surface, self.active_background, self.rect)
+            Label(self.surface, self.text, self.rect, self.active_color, self.font, align_horizontal=self.label_horizontal, align_vertical=self.label_vertical).draw()
         elif self.is_touching_mouse():
-            pygame.draw.rect(surface, self.hover_background, self.rect)
-            Label(surface, text, rect, self.hover_color, font, align_horizontal=label_horizontal, align_vertical=label_vertical).draw()
+            pygame.draw.rect(self.surface, self.hover_background, self.rect)
+            Label(self.surface, self.text, self.rect, self.hover_color, self.font, align_horizontal=self.label_horizontal, align_vertical=self.label_vertical).draw()
         elif self.disabled:
-            pygame.draw.rect(surface, self.disabled_background, self.rect)
-            Label(surface, text, rect, disabled_color, font, align_horizontal=label_horizontal, align_vertical=label_vertical).draw()
+            pygame.draw.rect(self.surface, self.disabled_background, self.rect)
+            Label(self.surface, self.text, self.rect, self.disabled_color, self.font, align_horizontal=self.label_horizontal, align_vertical=self.label_vertical).draw()
         else:
-            pygame.draw.rect(surface, self.background, self.rect)
-            Label(surface, text, rect, self.color, font, align_horizontal=label_horizontal, align_vertical=label_vertical).draw()
-
-    def update(self):
-        """
-        Updates the button
-        """
-        self.on_click()
+            pygame.draw.rect(self.surface, self.background, self.rect)
+            Label(self.surface, self.text, self.rect, self.color, self.font, align_horizontal=self.label_horizontal, align_vertical=self.label_vertical).draw()
+    def update(self) -> None:
+        self.check_click()
         self.draw()
-    def reload_theme(self):
+    def reload_theme(self) -> None:
         self.color = common.set_color("theme", "ACCENT_FOREGROUND")
         self.background = common.set_color("theme", "ACCENT")
         self.hover_color = common.set_color("theme", "ACCENT_FOREGROUND")
