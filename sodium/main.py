65879c2958475c528b3d27f6b6dc4c5bd3c53c3f
Enviction <schmidtjonas662@gmail.com>
2024-08-16 18:20:25 +0200

    removed docstring and added a default surface value as well as a bg_fill function

--- sodium/main.py
+++ sodium/main.py
@@ -1,11 +1,11 @@
-"""
-mainloop module
-"""
 import sodium
 from . import theme
 from . import Window
+from . import cache
 
-def mainloop(win, main, auto=False):
+def mainloop(win="wiscreen", main=None, auto=False):
+    if win == "wiscreen":
+        win = cache.screen
     if not auto:
         running = True
         while running:
@@ -27,5 +27,15 @@ def mainloop(win, main, auto=False):
 
     sodium.quit_window()
 
-def start(win, main):
+def start(win="wiscreen", main=None):
+    if win == "wiscreen":
+        win = cache.screen
     mainloop(win, main, True)
+
+def bg_fill(win="wiscreen", color=None):
+    if win == "wiscreen":
+        win = cache.screen
+    if color is None:
+        win.fill(theme.theme_colors["BACKGROUND"])
+    else:
+        win.fill(color)
